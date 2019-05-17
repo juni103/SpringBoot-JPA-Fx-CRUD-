28bf8be609577f593e2ee1ca9549f25f160f82d3
Junaid Iqbal <junaid.iqbal@emumba.com>
2019-05-17 22:23:57 +0500

    INITIAL COMMIT

--- src/main/java/com/example/demo/StageManager.java
+++ src/main/java/com/example/demo/StageManager.java
@@ -7,8 +7,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import com.example.demo.controller.HomeController;
 import com.example.demo.controller.SupplierController;
 import com.example.demo.controller.SupplierFormController;
+import com.example.demo.controller.form.CategoryFormController;
+import com.example.demo.controller.form.EmployeeFormController;
+import com.example.demo.controller.form.ProductFormController;
+import com.example.demo.controller.form.PurchaseFormController;
 import com.example.demo.model.Supplier;
 
 import javafx.fxml.FXMLLoader;
@@ -29,6 +34,10 @@ public class StageManager {
 	@Value("${classpath:/com/example/demo/view/SupplierFormView.fxml}")
 	private Resource supplierFormViewResource;
 	
+	@Value("${classpath:/com/example/demo/view/css/style.css}")
+	private Resource styleCss;
+	
+	@Autowired HomeController homeController;
 	@Autowired SupplierController supplierController;
 	
 	public void setPrimaryStage(Stage primaryStage) {
@@ -37,10 +46,11 @@ public class StageManager {
 
 	public void showMain() {
 		try {
-			FXMLLoader loader = new FXMLLoader(mainFxmlResource.getURL());
-			loader.setController(supplierController);
+			FXMLLoader loader = new FXMLLoader(homeController.getView());
+			loader.setController(homeController);
 			Parent root = loader.load();
 			Scene scene = new Scene(root);
+			scene.getStylesheets().addAll(styleCss.getURL().toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch (IOException e) {
@@ -72,5 +82,39 @@ public class StageManager {
 		}
 
 		return null;
+	}
+
+	public void showCategoryForm(Resource categoryFormViewResource, CategoryFormController controller) {
+		showForm(categoryFormViewResource, controller);
+
+	}
+
+	public void showEmployeeForm(Resource employeeFormViewResource, EmployeeFormController employeeFormController) {
+		showForm(employeeFormViewResource, employeeFormController);
+	}
+
+	public void showPurchaseForm(Resource purchaseFormViewResource, PurchaseFormController purchaseFormController) {
+		showForm(purchaseFormViewResource, purchaseFormController);
+	}
+
+	public void showProductForm(Resource productFormViewResource, ProductFormController productFormController) {
+		showForm(productFormViewResource, productFormController);
+	}
+
+	public void showForm(Resource fxmlResource, Object controllerObject) {
+		try {
+			Stage dialogStage = new Stage();
+			FXMLLoader loader = new FXMLLoader(fxmlResource.getURL());
+			loader.setController(controllerObject);
+
+			AnchorPane pane = loader.load();
+			Scene scene = new Scene(pane);
+			scene.getStylesheets().add(styleCss.getURL().toExternalForm());
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.setScene(scene);
+			dialogStage.showAndWait();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
